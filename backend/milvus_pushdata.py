ffd51b21bb90b62adc3cc3fb8501a067a12036cd
venky3692 <venkatesh.iyer@tarento.com>
2024-05-08 12:47:43 +0530

    Adding milvus code

---
+++ backend/milvus_pushdata.py
@@ -0,0 +1,31 @@
+from pymilvus import connections, Collection
+import mediapipe as mp
+from mediapipe.tasks import python
+from mediapipe.tasks.python import vision
+import os
+
+class push:
+    def __init__(self):
+        self.image_path = "/home/ubuntu/DPIIT/logos/"
+        self.collection = Collection("IMAGE_EMBEDDINGS")
+        self.base_options = python.BaseOptions(model_asset_path='embedder.tflite')
+        self.l2_normalize = True #@param {type:"boolean"}
+        self.quantize = True #@param {type:"boolean"}
+        self.options = vision.ImageEmbedderOptions(
+            base_options=self.base_options, l2_normalize=self.l2_normalize, quantize=self.quantize)
+
+    def push_data_to_milvus(self):
+        with vision.ImageEmbedder.create_from_options(self.options) as embedder:
+            embedding_list =[]
+            image_name_list = []
+            for image in os.listdir(self.image_path):
+                image_name = image.split('/')[-1]
+                embedding = embedder.embed(image)
+                embedding_list.append(embedding.embeddings[0].embedding)
+                image_name_list.append(image_name)
+                self.collection.insert([embedding_list, image_name_list])
+                embedding_list.clear()
+                image_name_list.clear()
+
+
+

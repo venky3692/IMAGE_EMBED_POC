1264ee822fc29cf3baf53d0e6643a9d9616befb4
venky3692 <venkatesh.iyer@tarento.com>
2024-05-16 16:27:54 +0530

    paddle ocr implementation

--- backend/text_extraction.py
+++ backend/text_extraction.py
@@ -1,8 +1,9 @@
-import pytesseract
-import cv2
+from paddleocr import PaddleOCR,draw_ocr
+ocr = PaddleOCR(use_angle_cls=True, lang='en')
+
 
 def extraction_of_text(original_image, fake_image):
-    get_text_from_original = pytesseract.image_to_string(cv2.imread(original_image))
-    get_text_from_fake = pytesseract.image_to_string(cv2.imread(fake_image))
+    get_text_from_original = ocr.ocr(original_image, cls=True)
+    get_text_from_fake = ocr.ocr(fake_image, cls=True)
         
     return get_text_from_original, get_text_from_fake 

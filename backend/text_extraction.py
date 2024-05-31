cce0bcf0bd01c8bc9cf0cb2b1439c8e6047e3e33
sanjay-v46 <sanjayvijaykumar@tarento.com>
2024-05-31 22:11:20 +0530

    updated readme

--- backend/text_extraction.py
+++ backend/text_extraction.py
@@ -8,17 +8,16 @@ def extraction_of_text(image):
     try:
         resized_img = cv2.resize(img, (300, 200), interpolation=cv2.INTER_AREA)
         get_text_from_image = ocr.ocr(resized_img, cls=True)
-        if get_text_from_image:
-            print(get_text_from_image[0][0][1][0].upper())
+        text = ''
+        print("get_text_from_image", get_text_from_image)
+        if get_text_from_image[0] is not None:
+            text = get_text_from_image[0][0][1][0].upper()
         
-        return get_text_from_image[0][0][1][0].upper()
+        return text;
     except Exception as e: 
         print("error", e);
-        get_text_from_image = ocr.ocr(img, cls=True)
-        if get_text_from_image:
-            print(get_text_from_image[0][0][1][0].upper())
         
-        return get_text_from_image[0][0][1][0].upper()
+        return '';
 # extraction_of_text('/home/sanjayvijaykumar/Documents/IMAGE_EMBED_POC/data_set/text_test/samsung-original.PNG')
 
 #    return get_text_from_original, get_text_from_fake 
--- readme.md
+++ readme.md
@@ -9,3 +9,20 @@ docker run \
 -v oracledata:/opt/oracle/oradata \
 container-registry.oracle.com/database/express:latest
 ```
+
+## Once the above db is setup
+```
+get inside docker container: sudo docker exec -it <docker-id> bash
+connect to db cli: sqlplus sys/Password1!@sys as sysdba
+
+Run below commands
+CREATE TABLE images (
+    id NUMBER GENERATED ALWAYS AS IDENTITY,
+    image_data BLOB,
+    image_name VARCHAR2(255),
+    CONSTRAINT image_pk PRIMARY KEY (image_id)
+);
+CREATE USER imageUser IDENTIFIED BY imageUser123;
+GRANT CONNECT TO imageUser;
+GRANT SELECT, INSERT, UPDATE, DELETE ON images TO imageUser;
+```
